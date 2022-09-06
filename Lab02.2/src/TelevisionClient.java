9d7e231d389253b59b30cfc8c9b94c2514823396
Wint-1234 <winton.ebanks@yahoo.com>
2022-09-06 18:05:10 -0400

    Completed Lab3.4.

--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -2,21 +2,34 @@ public class TelevisionClient {
 
   public static void main(String[] args) {
 
-    Television tv1 = new Television();
-    tv1.setBrand("Samsung");
-    tv1.setVolume(32);
-
-    Television tv2 = new Television();
-    tv2.setBrand("Sony");
-    tv2.setVolume(56);
+    Television tv1 = new Television("Samsung", 125);
+    //tv1.setBrand("Samsung");
+    //tv1.setVolume(32);
 
     tv1.turnOn();
     tv1.turnOff();
+    System.out.println(tv1);
+
+    System.out.printf("%d television instances created%n", Television.getInstanceCount());
+
+    Television tv2 = new Television("Sony", 56);
+    //tv2.setBrand("Sony");
+    //tv2.setVolume(56);
 
     tv2.turnOn();
     tv2.turnOff();
+    System.out.println(tv2);
 
-    System.out.println(tv1.toString());
-    System.out.println(tv1);
+    System.out.printf("%d television instances created%n", Television.getInstanceCount());
+
+
+    Television tv3 = new Television("LG");
+
+    tv3.turnOn();
+    tv3.turnOff();
+    System.out.println(tv3);
+
+    System.out.printf("%d television instances created%n", Television.getInstanceCount());
+
   }
 }
