9882fc280c680cd7f92bb4370a537c2795df5b76
Wint-1234 <winton.ebanks@yahoo.com>
2022-09-06 13:56:18 -0400

    Added verifyInternetConnection

--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -3,12 +3,12 @@ public class TelevisionClient {
   public static void main(String[] args) {
 
     Television tv1 = new Television();
-    tv1.brand = "Zenith";
-    tv1.volume = 25;
+    tv1.setBrand("Samsung");
+    tv1.setVolume(32);
 
     Television tv2 = new Television();
-    tv2.brand = "Sony";
-    tv2.volume = 56;
+    tv2.setBrand("Sony");
+    tv2.setVolume(56);
 
     tv1.turnOn();
     tv1.turnOff();
@@ -16,5 +16,7 @@ public class TelevisionClient {
     tv2.turnOn();
     tv2.turnOff();
 
+    System.out.println(tv1.toString());
+    System.out.println(tv1);
   }
 }
