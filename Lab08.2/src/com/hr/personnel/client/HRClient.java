40dfac698f358e09fed8c6e755db3077ff5a9c0e
Wint-1234 <winton.ebanks@yahoo.com>
2022-09-13 18:41:07 -0400

    Completed Lab08.2

--- Lab08.2/src/com/hr/personnel/client/HRClient.java
+++ Lab08.2/src/com/hr/personnel/client/HRClient.java
@@ -10,6 +10,8 @@ package com.hr.personnel.client;
 
 import com.hr.personnel.Department;
 import com.hr.personnel.Employee;
+import com.hr.personnel.HourlyEmployee;
+import com.hr.personnel.SalariedEmployee;
 import java.time.LocalDate;
 
 /**
@@ -25,6 +27,9 @@ class HRClient {
         // add Employees to it
         dept.addEmployee(new Employee("Jason", LocalDate.of(1990, 8, 24)));
         dept.addEmployee(new Employee("Julie", LocalDate.of(2000, 2, 2)));
+
+        dept.addEmployee(new SalariedEmployee("Eric", LocalDate.of(2022, 9, 13), 110000));
+        dept.addEmployee(new HourlyEmployee("Winton", LocalDate.of(2022, 1, 19), 53.13, 40));
 
         // list its Employees
         System.out.println("\nList employees:");
